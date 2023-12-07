1a09bbdaa500ce69b0ad9e11e6e407385bb9b052
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-12-07 14:30:53 -0300

    Refactor Garage component and add delete confirmation

--- src/components/Garage.js
+++ src/components/Garage.js
@@ -1,93 +1,114 @@
-// Garage.js
-import React, { useState, useEffect } from "react";
-import { StyleSheet, ScrollView, TouchableOpacity, Platform, Image, Alert } from "react-native";
-import { theme, withGalio, GalioProvider, Text, Block, NavBar, Button, Icon, } from 'galio-framework';
-import GarageService from '../services/garages';
-import { TouchableHighlight } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Platform,
+  Image,
+  Alert,
+} from "react-native";
+import {
+  theme,
+  withGalio,
+  GalioProvider,
+  Text,
+  Block,
+  NavBar,
+  Button,
+  Icon,
+} from "galio-framework";
+import GarageService from "../services/garages";
 
-class Garage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      garagesList: [],
-      error: null,
-    };
-  }
+class Garage extends Component {
+  state = {
+    garagesList: [],
+    error: null,
+    highlightedGarageId: null,
+  };
 
   async componentDidMount() {
     try {
       const garagesData = await GarageService.getAllGarages();
       this.setState({ garagesList: garagesData, error: null });
     } catch (error) {
-      console.error('Error fetching garages:', error);
-      this.setState({ error: 'Erro ao carregar garagens.' });
+      console.error("Error fetching garages:", error);
+      this.setState({ error: "Erro ao carregar garagens." });
     }
   }
 
-  confirmAndDeleteGarage = async (garage) => {
-    const confirmMessage = `Deseja realmente excluir a garagem "${garage.name}"?`;
-
+  confirmDeleteGarage = async (garage) => {
     Alert.alert(
-      'Confirmar Exclusão',
-      confirmMessage,
+      "Confirmar Exclusão",
+      `Deseja realmente excluir a garagem "${garage.name}"?`,
       [
         {
-          text: 'Cancelar',
-          style: 'cancel',
+          text: "Cancelar",
+          style: "cancel",
         },
         {
-          text: 'Excluir',
-          onPress: async () => await this.deleteGarage(garage),
+          text: "Excluir",
+          onPress: () => this.handleDeleteGarage(garage),
         },
       ],
       { cancelable: true }
     );
   };
 
-  deleteGarage = async (garage) => {
+  handleDeleteGarage = async (garage) => {
     try {
       await GarageService.deleteGarage(garage.id);
 
-      const updatedGarages = this.state.garagesList.filter(item => item.id !== garage.id);
+      const updatedGarages = this.state.garagesList.filter(
+        (item) => item.id !== garage.id
+      );
       this.setState({ garagesList: updatedGarages, error: null });
     } catch (error) {
-      console.error('Error deleting garage:', error);
-      this.setState({ error: 'Erro ao deletar garagem.' });
+      console.error("Error deleting garage:", error);
+      this.setState({ error: "Erro ao deletar garagem." });
     }
   };
 
   renderGarages = () => {
-    const { garagesList } = this.state;
+    const { garagesList, highlightedGarageId } = this.state;
     const { navigation } = this.props;
 
     return garagesList.map((garage, index) => (
       <Block key={index} style={styles.container}>
         <Block style={styles.card}>
-          <TouchableHighlight
-            underlayColor={theme.COLORS.MUTED} // Cor de fundo quando pressionado
-            onPress={() => navigation.navigate("Carros")}
-            style={styles.imageContainer}
-          >
+          <Block style={styles.imageContainer}>
             <Image style={styles.image} source={{ uri: garage.image.url }} />
-          </TouchableHighlight>
-          <TouchableOpacity
-            onPress={() => this.confirmAndDeleteGarage(garage)}
-            style={styles.deleteButton}
-          >
-            <Icon
-              name="trash"
-              family="feather"
-              size={theme.SIZES.BASE}
-              color={theme.COLORS.ERROR}
-              style={styles.deleteIcon}
-            />
-          </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.confirmDeleteGarage(garage)}
+              style={[
+                styles.deleteButton,
+                highlightedGarageId === garage.id &&
+                styles.highlightedDeleteButton,
+              ]}
+              onTouchStart={() =>
+                this.setState({ highlightedGarageId: garage.id })
+              }
+              onTouchEnd={() => this.setState({ highlightedGarageId: null })}
+            >
+              <Icon
+                name="trash"
+                family="feather"
+                size={theme.SIZES.BASE}
+                color={theme.COLORS.ERROR}
+                style={styles.deleteIcon}
+              />
+            </TouchableOpacity>
+          </Block>
           <Block style={styles.content}>
             <Block style={styles.titleContainer}>
-              <Text h4 style={styles.text}>{garage.name}</Text>
+              <Text h4 style={styles.text}>
+                {garage.name}
+              </Text>
             </Block>
             <Text style={styles.text2}>{garage.address}</Text>
-            <Button style={styles.button} onPress={() => navigation.navigate("Carros")}>
+            <Button
+              style={styles.button}
+              onPress={() => navigation.navigate("Carros")}
+            >
               Ver Carros
             </Button>
           </Block>
@@ -104,10 +125,10 @@ class Garage extends React.Component {
       <Block safe flex>
         <NavBar
           title="Minhas Garagens"
-          titleStyle={{ alignSelf: 'flex-start', marginLeft: 10, fontWeight: 'bold', color: theme.COLORS.ICON }}
+          titleStyle={styles.navTitle}
           leftIconColor={theme.COLORS.MUTED}
           rightIconColor={theme.COLORS.ICON}
-          left={(
+          left={
             <TouchableOpacity onPress={() => navigation.openDrawer()}>
               <Icon
                 name="menu"
@@ -116,8 +137,8 @@ class Garage extends React.Component {
                 color={theme.COLORS.ICON}
               />
             </TouchableOpacity>
-          )}
-          right={(
+          }
+          right={
             <Button
               onlyIcon
               icon="plus"
@@ -126,8 +147,8 @@ class Garage extends React.Component {
               color={theme.COLORS.ICON}
               onPress={() => navigation.navigate("Novo Carro")}
             />
-          )}
-          style={Platform.OS === 'android' ? { marginTop: theme.SIZES.BASE } : null}
+          }
+          style={Platform.OS === "android" ? styles.navBarAndroid : null}
         />
         <ScrollView>
           {error ? (
@@ -142,10 +163,32 @@ class Garage extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  navTitle: {
+    alignSelf: "flex-start",
+    marginLeft: 10,
+    fontWeight: "bold",
+    color: theme.COLORS.ICON,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    width: theme.SIZES.BASE * 20,
+    backgroundColor: theme.COLORS.WHITE,
+    marginVertical: theme.SIZES.BASE,
+    marginHorizontal: theme.SIZES.BASE * 2,
+    borderRadius: theme.SIZES.BASE,
+    shadowColor: "black",
+    shadowRadius: 4,
+    elevation: 2,
+  },
   imageContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
   },
   image: {
     width: theme.SIZES.BASE * 20,
@@ -160,54 +203,41 @@ const styles = StyleSheet.create({
     backgroundColor: theme.COLORS.ICON,
   },
   deleteButton: {
-    position: 'absolute',
+    position: "absolute",
     top: 10,
     right: 10,
+  },
+  highlightedDeleteButton: {
+    backgroundColor: "rgba(255, 0, 0, 0.2)",
   },
   deleteIcon: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    color: "red",
-  },
-  text: {
-    color: theme.COLORS.ICON,
-    fontWeight: 'bold',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  card: {
-    width: theme.SIZES.BASE * 20,
-    backgroundColor: theme.COLORS.WHITE,
-    marginVertical: theme.SIZES.BASE,
-    marginHorizontal: theme.SIZES.BASE * 2,
-    borderRadius: theme.SIZES.BASE,
-    shadowColor: 'black',
-    shadowRadius: 4,
-    elevation: 2,
-    position: 'relative',
+    color: theme.COLORS.ERROR,
   },
   content: {
     flex: 1,
     padding: theme.SIZES.BASE * 2,
-    flexDirection: 'column',
-    alignItems: 'center',
+    flexDirection: "column",
+    alignItems: "center",
   },
   titleContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: theme.SIZES.BASE,
+  },
+  text: {
+    color: theme.COLORS.ICON,
+    fontWeight: "bold",
   },
   text2: {
     color: theme.COLORS.ICON,
   },
   errorText: {
     color: theme.COLORS.ERROR,
-    textAlign: 'center',
+    textAlign: "center",
+    marginTop: theme.SIZES.BASE,
+  },
+  navBarAndroid: {
     marginTop: theme.SIZES.BASE,
   },
 });
